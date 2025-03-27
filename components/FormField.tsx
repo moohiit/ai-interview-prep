0151d21790eed47abe4c7b91068e1d9a50edfc96
Mohit Patel <pmohit645@gmail.com>
2025-03-27 23:38:15 +0530

    initialize project structure with Next.js, Tailwind CSS, and essential configurations

---
+++ components/FormField.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import {
+  FormControl,
+  FormDescription,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Control, Controller, FieldValues, Path } from "react-hook-form";
+
+interface FormFieldProps<T extends FieldValues> {
+  control: Control<T>;
+  name: Path<T>;
+  label: string;
+  placeholder: string;
+  type?: 'text' | 'email' | 'password' | 'file';
+  required?: boolean;
+}
+
+const FormField = <T extends FieldValues>({control, name, label, placeholder, type="text",required}:FormFieldProps<T>) => (
+  <Controller
+  control={control}
+    name={name}
+    render={({ field }) => (
+      <FormItem>
+        <FormLabel className="label">{ label }</FormLabel>
+        <FormControl>
+          <Input className="input" type={type} placeholder={placeholder} {...field} required={ required } />
+        </FormControl>
+        <FormMessage />
+      </FormItem>
+    )}
+  />
+);
+
+export default FormField;
