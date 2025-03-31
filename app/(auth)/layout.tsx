dd93346e492709a0f7eafa509637bbfd0d2fa83e
Mohit Patel <pmohit645@gmail.com>
2025-03-31 23:25:12 +0530

    refactor Agent component to handle call states and messages, update user info retrieval in interview page

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -4,8 +4,9 @@ import { ReactNode } from "react";
 
 const AuthLayout = async ({ children }: { children: ReactNode }) => {
   const isUserAuthenticated = await isAuthenticated();
-    if (isUserAuthenticated) redirect('/');
-  return <div className="auth-layout">{children}</div>;
+  if (isUserAuthenticated) redirect("/");
+  return <div className="auth-layout">{children}
+  </div>;
 };
 
 export default AuthLayout;
